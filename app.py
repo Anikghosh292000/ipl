09a8ac892f12cac4d020216ce787f47a153bf74a
Anikghosh292000 <154450409+Anikghosh292000@users.noreply.github.com>
2024-05-16 21:37:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title('IPL RUN PREDICTOR')
 
 sixth_th_run = st.number_input('6th_run', value=70, placeholder='Enter run after 6th over')
 first_st_wicket = st.number_input('1st_wicket', value=5, placeholder='Enter wicket after 6th over')
-fortheen_th_over = st.number_input('14th_over', value=150, placeholder='Enter run after 14th over')
+fortheen_th_over = st.number_input('14th_over', value=150, placeholder='Enter run after 16th over')
 second_nd_wicket = st.number_input('2nd_wicket', value=9, placeholder='Enter wicket after 14th over')
 
 loaded_model = pickle.load(open('ipl_pred.sav', 'rb'))
