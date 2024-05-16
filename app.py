047f08ab974a5f8ffa0d2ccf1fe4be2032ca9383
Anikghosh292000 <154450409+Anikghosh292000@users.noreply.github.com>
2024-05-16 21:48:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import pickle
 import streamlit as st
 st.title('IPL RUN PREDICTOR')
 
-sixth_th_run = st.number_input('6th_run', value=70, placeholder='Enter run after 6th over')
-first_st_wicket = st.number_input('1st_wicket', value=5, placeholder='Enter wicket after 6th over')
-fortheen_th_over = st.number_input('14th_over', value=150, placeholder='Enter run after 16th over')
-second_nd_wicket = st.number_input('2nd_wicket', value=9, placeholder='Enter wicket after 16th over')
+sixth_th_run = st.number_input('Enter run after 6th over', value=70, placeholder='Enter run after 6th over')
+first_st_wicket = st.number_input('Enter wicket after 6th over', value=5, placeholder='Enter wicket after 6th over')
+fortheen_th_over = st.number_input('Enter run after 16th over', value=150, placeholder='Enter run after 16th over')
+second_nd_wicket = st.number_input('Enter wicket after 16th over', value=9, placeholder='Enter wicket after 16th over')
 
 loaded_model = pickle.load(open('ipl_pred.sav', 'rb'))
 prediction =loaded_model.predict([[sixth_th_run, first_st_wicket, fortheen_th_over, second_nd_wicket]])
